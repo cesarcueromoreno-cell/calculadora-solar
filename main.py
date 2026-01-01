e59218c149eb5818dc60fc2a06ad912e6704090d
cesarcueromoreno-cell <cesarcueromoreno@gmail.com>
2025-12-31 19:29:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pandas as pd
 from fpdf import FPDF
 from datetime import datetime
 import os
+
 st.error("🚨 SI VES ESTO, EL CÓDIGO YA ACTUALIZÓ")
+
 # --- SISTEMA DE SEGURIDAD ---
 # 1. Pedimos la contraseña en la barra lateral
 password = st.sidebar.text_input("🔑 Ingresa la contraseña:", type="password")
