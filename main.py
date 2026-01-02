df103ac93f2631063eb3e9c8ad142e0621a08331
cesarcueromoreno-cell <cesarcueromoreno@gmail.com>
2026-01-02 08:52:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -127,6 +127,8 @@ st.markdown("---")
 
 # BARRA LATERAL
 # BARRA LATERAL (LOGO)
+st.sidebar.image("logo.png.JPG")
+
 # --- DATOS DEL PROYECTO (Ahora fuera del if para que siempre funcionen) ---
 cliente = st.text_input("Cliente", "Empresa SAS")
 
