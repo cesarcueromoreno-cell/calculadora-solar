a5886b03e886c691c26609088e5ef54df2266c5a
cesarcueromoreno-cell <cesarcueromoreno@gmail.com>
2026-01-03 15:55:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -566,17 +566,17 @@ with tab3:
     
     # 1. Recuperamos las coordenadas para ponerlas en el reporte
     coords_pdf = coordenadas_ciudades.get(ciudad, coordenadas_ciudades["Colombia"])
-        # --- BIBLIOTECA DE INGENIERÍA Y RENDIMIENTO (ESTILO PVSYST) ---
-        precios = {
-            "panel": 625000,
-            "inversor_kw": 880000,
-            "estructura": 115000,
-            "mano_obra_w": 780,
-            "tramite": 1350000,
-            "eficiencia_inv": 0.97,
-            "perdidas_dc": 0.08,
-            "perdidas_temp": 0.05
-        }
+    # --- BIBLIOTECA DE INGENIERÍA Y RENDIMIENTO (ESTILO PVSYST) ---
+    precios = {
+        "panel": 625000,
+        "inversor_kw": 880000,
+        "estructura": 115000,
+        "mano_obra_w": 780,
+        "tramite": 1350000,
+        "eficiencia_inv": 0.97,
+        "perdidas_dc": 0.08,
+        "perdidas_temp": 0.05
+    }
     #---Este bloque reemplaza el contenido dentro de info_sistema_txt
         info_sistema_txt = f"""
 1. BALANCE ENERGETICO Y RENDIMIENTO (ESTILO PVSYST)
